6ce789633e8298dac5359c5578c53f426f7b1ec5
Richard Blundell <rblundell@deloitte.com>
2020-07-09 08:51:58 -0400

    add git ignore

--- delete_object_from_git.sh
+++ delete_object_from_git.sh
@@ -1,4 +1,23 @@
 #!/bin/bash -e
-cmd="git filter-branch --tree-filter \"rm -f $1\" -- --all"
+FILES_TO_DELETE="$@"
 
-echo -e $cmd
+setup(){
+    execution_dir="$(realpath "$(pwd)")"
+    base_dir="$( cd "$( dirname "$(realpath "${BASH_SOURCE[0]}")" )" && pwd )"
+    source $base_dir/.ansi.sh
+    source $base_dir/constants.sh
+    source $base_dir/utils.sh
+    STARTED_TS=$(_ts)
+}
+
+
+rm_file(){
+    ansi --yellow --underline "Deleting Files '$FILES_TO_DELETE' from GIT Objects...."
+    cmd="git filter-branch -f --tree-filter \"rm -f $FILES_TO_DELETE\" -- --all"
+    [[ "$EXECUTE_RM_CMD" == "1" ]] && eval $cmd && return
+    ansi --yellow --underline "$cmd"
+    
+}
+
+setup
+time rm_file
--- gitlistobjectbysize.sh
+++ gitlistobjectbysize.sh
@@ -1,11 +1,4 @@
 #!/bin/bash -e
-execution_dir="$(realpath "$(pwd)")"
-base_dir="$( cd "$( dirname "$(realpath "${BASH_SOURCE[0]}")" )" && pwd )"
-source $base_dir/.ansi.sh
-source $base_dir/constants.sh
-source $base_dir/utils.sh
-STARTED_TS=$(_ts)
-
 
 clean_up(){
     ENDED_TS=$(_ts)
@@ -16,24 +9,51 @@ clean_up(){
         ansi --yellow --bold "Removing temp file $tempFile of $file_lines lines and $(humanize_bytes $file_bytes) after ${DURATION_MS}ms." && \
         unlink "$tempFile"        
     [[ -f "$execution_dir/.gitignore" ]] && \
-        grep -q "$RESULTS_FILENAME" "$execution_dir/.gitignore" && \
-        ansi --yellow "Adding $RESULTS_FILENAME to $execution_dir/.gitignore"
+        grep -q "$RESULTS_FILENAME" "$execution_dir/.gitignore" || \
+            { ansi --yellow "Adding $RESULTS_FILENAME to $execution_dir/.gitignore" && \
+                echo -e "$RESULTS_FILENAME" >> "$execution_dir/.gitignore"; }
 }
 
-tempFile=$(mktemp)
-trap clean_up EXIT
+setup(){
+    execution_dir="$(realpath "$(pwd)")"
+    base_dir="$( cd "$( dirname "$(realpath "${BASH_SOURCE[0]}")" )" && pwd )"
+    source $base_dir/.ansi.sh
+    source $base_dir/constants.sh
+    source $base_dir/utils.sh
+    STARTED_TS=$(_ts)
+    tempFile=$(mktemp)
+    trap clean_up EXIT
+    IFS=$'\n'
+}
 
+collect_commit_hashes(){
+    for commitSHA1 in $(git rev-list --all); do
+	    git ls-tree -r --long "$commitSHA1" >>"$tempFile"
+    done
+}
 
-IFS=$'\n'
-
-for commitSHA1 in $(git rev-list --all); do
-	git ls-tree -r --long "$commitSHA1" >>"$tempFile"
-done
-
-time (sort --key 3 "$tempFile" | \
+sort_hash_sizes(){
+  sort --key 3 "$tempFile" | \
 	uniq | \
 	sort --key 4 --numeric-sort --reverse | \
-    tee $execution_dir/.git_objects_sorted_by_size.txt | \
-    head -n $DISPLAY_RESULTS_QTY)
+    tee $execution_dir/.git_objects_sorted_by_size.txt
+}
 
-rm "$tempFile"
+limit_results(){
+    head -n $DISPLAY_RESULTS_QTY
+}
+
+format_results(){
+     tr -s ' ' |   sed 's/[[:space:]]/ /g'  | cut -d' ' -f3,4,5
+}
+
+main(){
+    setup
+    collect_commit_hashes
+    sort_hash_sizes | \
+        limit_results | \
+        format_results
+}
+
+
+main
