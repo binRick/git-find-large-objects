10edcc9f987094cf5d43db1ddb74b1dc29e977cd
Richard Blundell <rblundell@deloitte.com>
2020-07-09 08:08:23 -0400

    add git ignore

--- gitlistobjectbysize.sh
+++ gitlistobjectbysize.sh
@@ -5,8 +5,9 @@ for commitSHA1 in $(git rev-list --all); do
 	git ls-tree -r --long "$commitSHA1" >>"$tempFile"
 done
 
-sort --key 3 "$tempFile" | \
+time sort --key 3 "$tempFile" | \
 	uniq | \
-	sort --key 4 --numeric-sort --reverse
+	sort --key 4 --numeric-sort --reverse | \
+    tee $(pwd)/.git_objects_sorted_by_size.txt
 
 rm "$tempFile"
